1fe6d89db6f7e6d74a41118b4c194d8aa0624cc2
Wes Johnson <wjohnson@wealthsimple.com>
2020-10-13 21:22:56 -0400

    add jest-snapshot-resolver pkg

---
+++ packages/jest-snapshot-resolver/src/index.ts
@@ -0,0 +1,64 @@
+import path from "path";
+import glob from "glob";
+
+const defaultOptions = {
+  ignore: ["**/node_modules/**", "**/ios/**", "**/android/**"],
+};
+
+export const resolveAllSnapshotPaths = (): Promise<string[]> => {
+  return new Promise((resolve, reject) => {
+    glob("**/*.snap", defaultOptions, function (err, files) {
+      if (err) return reject(err);
+      resolve(files);
+    });
+  });
+};
+
+const cwd = process.cwd();
+const pathRelativeToCwd = (modulePath: string) => path.resolve(cwd, modulePath);
+const sidekickTest = /source\": \"react-test-render-transformer/;
+
+const createSnapshotFilter = (valuePattern: RegExp = sidekickTest) => (
+  modulePath: string
+): boolean => {
+  const snapshot = require(pathRelativeToCwd(modulePath));
+  const match = Object.keys(snapshot).find((name) => {
+    const snapshotValue = snapshot[name];
+    return valuePattern.test(snapshotValue);
+  });
+
+  return !!match;
+};
+
+type Snapshot = {
+  path: string;
+  name: string;
+  value: any;
+};
+
+type SnapshotMap = {
+  [path: string]: Snapshot[];
+};
+
+const parseSnapshotValue = (value: string): any => {
+  try {
+    return JSON.parse(value.replace(/^\s+Array\s+/, ""));
+  } catch (e) {
+    return null;
+  }
+};
+
+export const gatherSnapshots = (modulePaths: string[]): SnapshotMap => {
+  return modulePaths
+    .filter(createSnapshotFilter())
+    .reduce((acc, modulePath) => {
+      const snapshotModule = require(pathRelativeToCwd(modulePath));
+      const snapshots = Object.keys(snapshotModule).map((key) => ({
+        path: modulePath,
+        name: key,
+        value: parseSnapshotValue(snapshotModule[key]),
+      }));
+
+      return { ...acc, [modulePath]: snapshots };
+    }, {});
+};
