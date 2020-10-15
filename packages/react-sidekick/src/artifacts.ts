c6463592d135f91995fafd7ca7f4b47984d7e92b
Wes Johnson <wjohnson@wealthsimple.com>
2020-10-14 22:53:55 -0400

    avoid overwriting different snapshots in beforeEach

--- packages/react-sidekick/src/artifacts.ts
+++ packages/react-sidekick/src/artifacts.ts
@@ -1,3 +1,4 @@
+import { statSync } from "fs";
 import { exec } from "./utils/exec";
 import { getProjectPath } from "./utils/paths";
 import { Frame } from "./utils/stack-frame-parser";
@@ -23,14 +24,44 @@ const getBaseArtifactPath = () => {
   return `${baseProjectPath}/${artifactFolder}`;
 };
 
+type Platform = "ios" | "android";
+
+const getFilename = (
+  folder: string,
+  context: Frame,
+  platform: Platform
+): string => {
+  let baseFilename = `${context.lineNumber}-${context.column}-${platform}`;
+  const makeFullPath = (offset?: number): [string, string] => [
+    `${getBaseArtifactPath()}/${folder}/`,
+    `${baseFilename}${offset ? `-${offset}` : ""}.png`,
+  ];
+  let nextPath = makeFullPath();
+  let exists = true;
+  let count = 0;
+  while (exists) {
+    try {
+      console.log(`checking screenshot path ${nextPath}`);
+      statSync(nextPath.join(""));
+      count++;
+      nextPath = makeFullPath(count);
+      continue;
+    } catch {
+      exists = false;
+    }
+  }
+
+  return nextPath[1];
+};
+
 export const getScreenshotFilePath = (
   context: Frame,
-  platform: "ios" | "android" = "ios"
+  platform: Platform = "ios"
 ): ArtifactLocation => {
   const folder = makeFolderPath(getRelativeProjectPath(context.filePath));
   return {
     folder,
-    filename: `${context.lineNumber}-${context.column}-${platform}.png`,
+    filename: getFilename(folder, context, platform),
   };
 };
 
