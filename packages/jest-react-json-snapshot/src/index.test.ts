a6169329b19de3b99acfd5f48af978ab97daa98d
Wes Johnson <wjohnson@wealthsimple.com>
2021-05-02 22:32:52 -0400

    rm unused packages

--- packages/jest-react-json-snapshot/src/index.test.ts
+++
@@ -1,9 +0,0 @@
-import { simpleFixture } from "./fixtures/simple.fixture";
-
-describe("jest react json snapshot serializer", () => {
-  describe("simple tree", () => {
-    it("should produce a JSON snapshot w/ callsite info", () => {
-      expect(simpleFixture).toMatchSnapshot();
-    });
-  });
-});
